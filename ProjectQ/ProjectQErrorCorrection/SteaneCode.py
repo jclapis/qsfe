69b942d5228c5e1ed58b2e264bae8c5b7ad466c6
Joe Clapis <jclapis@mitre.org>
2019-05-24 12:52:28 -0400

    Implemented the Shor and Steane ECCs in ProjectQ. They worked perfectly and simulated very quickly.

--- ProjectQ/ProjectQErrorCorrection/SteaneCode.py
+++ ProjectQ/ProjectQErrorCorrection/SteaneCode.py
@@ -17,9 +17,10 @@
 
 from ecc_test_implementation import run_tests
 import unittest
-from pyquil import Program
-from pyquil.quilatom import QubitPlaceholder
-from pyquil.gates import *
+from projectq import MainEngine
+from projectq.ops import *
+from projectq.meta import Dagger, Control
+from utility import reset
 
 
 class SteaneCode(unittest.TestCase):
@@ -43,12 +44,9 @@ class SteaneCode(unittest.TestCase):
 	    protect against bit and/or phase flips.
 
         Parameters:
-            qubits (list[QubitPlaceholder]): The register that will become the logical 
+            qubits (Qureg): The register that will become the logical 
                 error-protected qubit. The original qubit can be in any state, but it
                 must be the first element. All of the other qubits must be |0>.
-
-        Returns:
-            A program that creates an error-protected qubit.
 
         Remarks:
             The circuit for this preparation (called the encoding circuit) can be seen
@@ -60,33 +58,27 @@ class SteaneCode(unittest.TestCase):
         # really recommend you read the papers on this code to understand why it works,
         # because it's a very cool idea - it's essentially a quantum version of a classical
         # Hamming code used in normal signal processing.
-        program = Program()
-
         for i in [4, 5, 6]:
-            program += H(qubits[i])
+            H | qubits[i]
         for i in [1, 2]:
-            program += CNOT(qubits[0], qubits[i])
+            CNOT | (qubits[0], qubits[i])
         for i in [0, 1, 3]:
-            program += CNOT(qubits[6], qubits[i])
+            CNOT | (qubits[6], qubits[i])
         for i in [0, 2, 3]:
-            program += CNOT(qubits[5], qubits[i])
+            CNOT | (qubits[5], qubits[i])
         for i in [1, 2, 3]:
-            program += CNOT(qubits[4], qubits[i])
-
-        return program
+            CNOT | (qubits[4], qubits[i])
 
 
-    def detect_bit_flip_error(self, program, qubits, parity_qubits, parity_measurement):
+    def detect_bit_flip_error(self, qubits):
         """
         Detects which physical qubit (if any) in the logical qubit was flipped.
 
         Parameters:
-            program (Program): The program to add the error detection to
-            qubits (list[QubitPlaceholder]): The logical error-encoded qubit
-            parity_qubits (list[QubitPlaceholder]): The ancilla qubits to use when determining
-                which qubit was flipped
-            parity_measurement (MemoryReference): The classical register used to
-                measure the parity qubits
+            qubits (Qureg): The logical error-encoded qubit
+
+        Returns:
+            An array of syndrome measurement results.
         """
 
 		# With 7 qubits, there are 8 possible error states: one for nothing being
@@ -97,29 +89,34 @@ class SteaneCode(unittest.TestCase):
 		# block, 3 belong to 2 blocks, and the last belongs to all 3 blocks. That way, you
 		# can turn the ancilla measurements into the 3-bit binary number that tells you exactly
 		# which qubit is broken, and flip it accordingly.
+        
+        parity_qubits = qubits.engine.allocate_qureg(3)
 
         for i in [0, 2, 4, 6]:  # Block 0: 0, 2, 4, 6
-            program += CNOT(qubits[i], parity_qubits[2])
+            CNOT | (qubits[i], parity_qubits[2])
         for i in [1, 2, 5, 6]:  # Block 1: 1, 2, 5, 6
-            program += CNOT(qubits[i], parity_qubits[1])
+            CNOT | (qubits[i], parity_qubits[1])
         for i in [3, 4, 5, 6]:  # Block 2: 3, 4, 5, 6
-            program += CNOT(qubits[i], parity_qubits[0])
+            CNOT | (qubits[i], parity_qubits[0])
 
+        parity_measurements = [0, 0, 0]
         for i in range(0, 3):
-            program += MEASURE(parity_qubits[i], parity_measurement[i])
+            Measure | parity_qubits[i]
+            parity_measurements[i] = int(parity_qubits[i])
+        
+        del parity_qubits
+        return parity_measurements
 
 
-    def detect_phase_flip_error(self, program, qubits, parity_qubits, parity_measurement):
+    def detect_phase_flip_error(self, qubits):
         """
         Detects which physical qubit (if any) had its phase flipped.
 
         Parameters:
-            program (Program): The program to add the detection gates to
             qubits (list[QubitPlaceholder]): The logical error-encoded qubit
-            parity_qubits (list[QubitPlaceholder]): The ancilla qubits to use when determining
-                which qubit had its phase flipped
-            parity_measurement (MemoryReference): The classical register used to
-                measure the parity qubits
+
+        Returns:
+            An array of syndrome measurement results.
         """
         
 		# The rationale here is the same as the bit flip detection above, with two key
@@ -136,33 +133,36 @@ class SteaneCode(unittest.TestCase):
 		# You'll end up with |10>.
 		# Entanglement is black magic. Fun fact: this property is why phase queries work, and
 		# how superdense coding actually does something useful.
-
-        for qubit in parity_qubits:
-            program += H(qubit)
-        for i in [0, 2, 4, 6]:  # Block 0: 0, 2, 4, 6
-            program += CNOT(parity_qubits[2], qubits[i])
-        for i in [1, 2, 5, 6]:  # Block 1: 1, 2, 5, 6
-            program += CNOT(parity_qubits[1], qubits[i])
-        for i in [3, 4, 5, 6]:  # Block 2: 3, 4, 5, 6
-            program += CNOT(parity_qubits[0], qubits[i])
-        for qubit in parity_qubits:
-            program += H(qubit)
         
+        parity_qubits = qubits.engine.allocate_qureg(3)
+
+        All(H) | parity_qubits
+        for i in [0, 2, 4, 6]:  # Block 0: 0, 2, 4, 6
+            CNOT | (parity_qubits[2], qubits[i])
+        for i in [1, 2, 5, 6]:  # Block 1: 1, 2, 5, 6
+            CNOT | (parity_qubits[1], qubits[i])
+        for i in [3, 4, 5, 6]:  # Block 2: 3, 4, 5, 6
+            CNOT | (parity_qubits[0], qubits[i])
+        All(H) | parity_qubits
+        
+        parity_measurements = [0, 0, 0]
         for i in range(0, 3):
-            program += MEASURE(parity_qubits[i], parity_measurement[i])
+            Measure | parity_qubits[i]
+            parity_measurements[i] = int(parity_qubits[i])
+        
+        del parity_qubits
+        return parity_measurements
 
 
-    def generate_classical_control_corrector(self, program, qubits, parity_measurement, gate):
+    def correct_error(self, qubits, parity_measurements, gate):
         """
-        Adds the error correction branches to the program, based on the parity measurements.
+        Corrects an error with the specified gate, based on the syndrome parity measurements.
 
         Parameters:
-            program (Program): The program being constructed
-            qubits (list[QubitPlaceholder]): The logical error-encoded qubit
-            parity_measurement (MemoryReference): The classical register used to
-                measure the parity qubits. This should already have the measurement
-                results stored in it.
-            gate (function): The gate to apply to the broken qubit in order to fix it
+            qubits (Qureg): The block of 3 qubits to correct
+            parity_measurmeents (list[int]): An int list containing the syndrome measurements
+                for the qubits
+            gate (Gate): The gate to apply to the broken qubit
         """
         
         # The 3 parity qubits used during the bit error and phase error detections will
@@ -184,55 +184,35 @@ class SteaneCode(unittest.TestCase):
         # In pyQuil, we can do this by switching on each of the 3 measurements, so we have
         # to build branches to handle each switch.
 
-        # If the first two measurements were 00
-        branch_00x = Program()
-        branch_00x.if_then(parity_measurement[2], gate(qubits[0]), Program())
+        # Convert the measurements to an int - this is basically ported from my Q# implementation
+        # of Steane's code. Look how much easier this is when we have classical-code-in-the-middle
+        # support! Compare this to the Qiskit and Forest implementations - this is so much more
+        # elegant.
+        broken_index = 0
+        broken_index = broken_index | parity_measurements[2]
+        broken_index = broken_index | (parity_measurements[1] << 1)
+        broken_index = broken_index | (parity_measurements[0] << 2)
 
-        # If the first two were 01
-        branch_01x = Program()
-        branch_01x.if_then(parity_measurement[2], gate(qubits[2]), gate(qubits[1]))
-        
-        # If the first two were 10
-        branch_10x = Program()
-        branch_10x.if_then(parity_measurement[2], gate(qubits[4]), gate(qubits[3]))
-        
-        # If the first two were 11
-        branch_11x = Program()
-        branch_11x.if_then(parity_measurement[2], gate(qubits[6]), gate(qubits[5]))
-        
-        # If the first measurement was 0
-        branch_0xx = Program()
-        branch_0xx.if_then(parity_measurement[1], branch_01x, branch_00x)
-        
-        # If the first measurement was 1
-        branch_1xx = Program()
-        branch_1xx.if_then(parity_measurement[1], branch_11x, branch_10x)
-
-        # Main branch (on the first measurement)
-        program.if_then(parity_measurement[0], branch_1xx, branch_0xx)
+        broken_index = broken_index - 1
+        if broken_index >= 0:
+            gate | qubits[broken_index]
 
 
-    def correct_errors(self, program, qubits):
+    def correct_errors(self, qubits):
         """
         Corrects any errors that have occurred within the logical qubit register.
 
         Parameters:
-            program (Program): The program to add the error correction to
-            qubits (list[QubitPlaceholder]): The logical qubit register to check and correct
+            qubits (Qureg): The logical qubit register to check and correct
         """
-        
-        parity_qubits = QubitPlaceholder.register(3)
-        parity_measurement = program.declare("parity_measurement", "BIT", 3)
 
         # Correct bit flips
-        self.detect_bit_flip_error(program, qubits, parity_qubits, parity_measurement)
-        self.generate_classical_control_corrector(program, qubits, parity_measurement, X)
-        for qubit in parity_qubits:
-            program += RESET(qubit)
+        parity_measurements = self.detect_bit_flip_error(qubits)
+        self.correct_error(qubits, parity_measurements, X)
 
         # Correct phase flips
-        self.detect_phase_flip_error(program, qubits, parity_qubits, parity_measurement)
-        self.generate_classical_control_corrector(program, qubits, parity_measurement, Z)
+        parity_measurements = self.detect_phase_flip_error(qubits)
+        self.correct_error(qubits, parity_measurements, Z)
 
             
     # ====================
@@ -253,11 +233,10 @@ class SteaneCode(unittest.TestCase):
         """
 
         number_of_qubits = 7
-        number_of_parity_qubits = 3
         number_of_random_tests = 25
         try:
-            run_tests(description, number_of_qubits, number_of_parity_qubits,
-                     number_of_random_tests, self, enable_bit_flip, enable_phase_flip)
+            run_tests(description, number_of_qubits, number_of_random_tests,
+                      self, enable_bit_flip, enable_phase_flip)
         except ValueError as error:
             self.fail(repr(error))
 
---
+++ ProjectQ/ProjectQErrorCorrection/shor_code.py
@@ -0,0 +1,264 @@
+# ========================================================================
+# Copyright (C) 2019 The MITRE Corporation.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
+
+
+from ecc_test_implementation import run_tests
+import unittest
+from projectq import MainEngine
+from projectq.ops import *
+from projectq.meta import Dagger, Control
+from utility import reset
+
+
+class ShorCode(unittest.TestCase):
+    """
+    This class implements Shor's error correction code. It employs 8 extra
+    qubits to protect 1 original qubit, and can recover from one bit flip
+    and/or one phase flip (not necessarily on the same qubit).
+    See the paper at https://arxiv.org/pdf/0905.2794.pdf for more details.
+    """
+    
+
+    # ==============================
+	# == Algorithm Implementation ==
+	# ==============================
+
+
+    def encode_register(self, qubits):
+        """
+        Creates an error-protected qubit, wrapping the original with 8 spares that
+        protect against bit and/or phase flips.
+
+        Parameters:
+            qubits (Qureg): The register that will become the logical 
+                error-protected qubit. The original qubit can be in any state, but it
+                must be the first element. All of the other qubits must be |0>.
+        """
+
+        # Copy q0 into q3 and q6 - these 3 qubits will form 3 "blocks" of qubits
+        for i in [3, 6]:
+            CNOT | (qubits[0], qubits[i])
+
+        # Give q1 and q2 the same phase as q0, and repeat for the other two blocks
+        for i in [0, 3, 6]:
+            H | qubits[i]
+            CNOT | (qubits[i], qubits[i + 1])
+            CNOT | (qubits[i], qubits[i + 2])
+
+
+    def detect_bit_flip_error(self, block, engine):
+        """
+        Detects which qubit (if any) in the given block was flipped.
+
+        Parameters:
+            block (list[Qureg]): The block of 3 qubits to check for bit flips
+            engine (Engine): The engine that's running the program
+            
+        Returns:
+            A tuple containing the syndrome measurement parity values of the first and second
+            qubits, and the first and third qubits respectively.
+
+        Remarks:
+            This is essentially just the 3-qubit bit flip code. For an explanation of what
+            this parity measurement is doing and how it works, check that code first.
+        """
+        
+        parity_qubits = engine.allocate_qureg(2)
+
+        # Check if q0 and q1 have the same value
+        CNOT | (block[0], parity_qubits[0])
+        CNOT | (block[1], parity_qubits[0])
+        
+        # Check if q0 and q2 have the same value
+        CNOT | (block[0], parity_qubits[1])
+        CNOT | (block[2], parity_qubits[1])
+
+        # Measure the parity values
+        Measure | parity_qubits[0]
+        Measure | parity_qubits[1]
+        parity_01 = int(parity_qubits[0])
+        parity_02 = int(parity_qubits[1])
+
+        # Delete the parity qubits since we don't need them anymore, and return the measurements
+        del parity_qubits
+        return (parity_01, parity_02)
+
+
+    def detect_phase_flip_error(self, qubits):
+        """
+        Detects which block (if any) had its phase flipped.
+
+        Parameters:
+            qubits (Qureg): The logical quantum register to check for errors
+            
+        Returns:
+            A tuple containing the syndrome measurement parity values of the first and second
+            qubits, and the first and third qubits respectively.
+        """
+        
+        parity_qubits = qubits.engine.allocate_qureg(2)
+
+        # Bring the register from the Z basis to the X basis so we can measure the
+        # phase differences between blocks
+        All(H) | qubits
+
+        # Compare the phases of all 6 qubits from the 1st and 2nd blocks. If any of the
+        # qubits in a block had its phase flipped, the entire block will show a phase
+        # flip. Like the bit flip measurements, this parity qubit will show a 1 if 
+        # the 1st and 2nd blocks have different phases.
+        for i in range(0, 6):
+            CNOT | (qubits[i], parity_qubits[0])
+
+        # Do the phase parity measurement for the 1st and 3rd blocks.
+        for i in [0, 1, 2, 6, 7, 8]:
+            CNOT | (qubits[i], parity_qubits[1])
+
+        # Put the qubits back into the Z basis
+        All(H) | qubits
+        
+        # Measure the parity values
+        Measure | parity_qubits[0]
+        Measure | parity_qubits[1]
+        parity_01 = int(parity_qubits[0])
+        parity_02 = int(parity_qubits[1])
+
+        # Delete the parity qubits since we don't need them anymore, and return the measurements
+        del parity_qubits
+        return (parity_01, parity_02)
+
+
+    def correct_error(self, qubits, parity_measurements, error_name, gate):
+        """
+        Corrects an error with the specified gate, based on the syndrome parity measurements.
+
+        Parameters:
+            qubits (Qureg): The block of 3 qubits to correct
+            parity_measurmeents (int, int): A tuple containing the syndrome measurements
+                for the block
+            error_name (str): The name of the error that could be on a qubit ("bit" or "phase")
+            gate (Gate): The gate to apply to the broken qubit
+        """
+
+        # This is the same as the "correct errors" function in the bit flip code.
+        (parity_01, parity_02) = parity_measurements
+        if parity_01 == 1:
+            if parity_02 == 1:
+                # The print statements need to be commented out because they print so many
+                # times that it actually breaks Visual Studio's test runner. Bummer.
+                # At least we know they work, thanks to the bit flip code!
+                # print(f"Detected {error_name} flip on qubit 0, correcting it.")
+                gate | qubits[0]
+            else:
+                gate | qubits[1]
+        else:
+            if parity_02 == 1:
+                gate | qubits[2]
+
+
+    def correct_errors(self, qubits):
+        """
+        Corrects any errors that have occurred within the logical qubit register.
+
+        Parameters:
+            qubits (Qureg): The logical qubit register to check and correct
+        """
+        
+        # Correct bit flips on the first block - look at the 3-qubit Bit Flip code for an
+        # explanation of how the classical register's output maps to the qubit to flip.
+        block_0 = [qubits[0], qubits[1], qubits[2]]
+        parity_measurements = self.detect_bit_flip_error(block_0, qubits.engine)
+        self.correct_error(block_0, parity_measurements, "bit", X)
+
+        # Correct bit flips on the second block
+        block_1 = [qubits[3], qubits[4], qubits[5]]
+        parity_measurements = self.detect_bit_flip_error(block_1, qubits.engine)
+        self.correct_error(block_1, parity_measurements, "bit", X)
+
+        # Correct bit flips on the third block
+        block_2 = [qubits[6], qubits[7], qubits[8]]
+        parity_measurements = self.detect_bit_flip_error(block_2, qubits.engine)
+        self.correct_error(block_2, parity_measurements, "bit", X)
+
+        # Correct any phase flips. Flipping any qubit in the broken block will end up putting
+        # the entire block back into the correct phase, so I just pick the first qubit of each one.
+        phase_block = [qubits[0], qubits[3], qubits[6]]
+        parity_measurements = self.detect_phase_flip_error(qubits)
+        self.correct_error(phase_block, parity_measurements, "phase", Z)
+
+
+    # ====================
+	# == Test Case Code ==
+	# ====================
+
+
+    def run_shor_test(self, description, enable_bit_flip, enable_phase_flip):
+        """
+        Runs a collection of tests on the Shor ECC.
+
+        Parameters:
+            description (name): A description for this batch of tests
+            enable_bit_flip (bool): True to run the tests where bit flip errors
+                are involved, False to leave bit flips off
+            enable_phase_flip (bool): True to run the tests where phase flip errors
+                are involved, False to leave phase flips off
+        """
+
+        number_of_qubits = 9
+        number_of_random_tests = 25
+        try:
+            run_tests(description, number_of_qubits, number_of_random_tests,
+                      self, enable_bit_flip, enable_phase_flip)
+        except ValueError as error:
+            self.fail(repr(error))
+            
+
+    def test_no_flip(self):
+        """
+        Runs the Shor ECC on all of the test cases without actually flipping
+        anything. This is helpful to make sure the test harness works as
+        intended when no errors are introduced.
+        """
+
+        self.run_shor_test("normal (no error)", False, False)
+
+
+    def test_bit_flip(self):
+        """
+        Runs the Shor ECC on all of the test cases with bit-flipping enabled.
+        """
+
+        self.run_shor_test("bit flip", True, False)
+
+
+    def test_phase_flip(self):
+        """
+        Runs the Shor ECC on all of the test cases with phase-flipping enabled.
+        """
+
+        self.run_shor_test("phase flip", False, True)
+
+
+    def test_combo(self):
+        """
+        Runs the Shor ECC on all of the test cases with both bit-flipping and 
+        phase-flipping enabled.
+        """
+
+        self.run_shor_test("combo", True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
